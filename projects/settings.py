a99a12231ee1b68de4622000999de8c627a3f867
Robert Kleyn <zeonita@gmail.com>
2024-05-13 06:19:58 +0200

    chore: Update settings.py with database URL configuration and debug mode

--- projects/settings.py
+++ projects/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-#import dj_database_url
+import dj_database_url
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -22,10 +22,7 @@ import django_heroku
 from decouple import config
 config.encoding = 'cp1251'
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-
+DEBUG = '1'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
